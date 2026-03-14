8f308925cb9d2586008f2543f6e39b21965c039f
theHashier <eugengaibu@gmail.com>
2026-03-14 14:21:48 +0100

    feat(pinia): implement global counter store using pinia with increment and decrement actions

---
+++ 09-pinia/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useCounterStore } from "./stores/counter"
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <main>
+    <h1>Pinia Global Counter</h1>
+
+    <h2>{{ counter.count }}</h2>
+
+    <button @click="counter.increment()">+</button>
+    <button @click="counter.decrement()">-</button>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 500px;
+  margin: 40px auto;
+  text-align: center;
+  background: lightblue;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 16px;
+}
+</style>
